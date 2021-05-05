f8fb9bbecd4b9b588d52447401064f50958cc7e2
Mihail2021 <78846335+Mihail2021@users.noreply.github.com>
2021-05-05 15:24:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,76 +1,69 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int *arr, int len, int value) {
-int c1 = 0;
-int i;
-int j;
-int l = len - 1;	
-for(i = 0; i < l; i++) {
-for(j = i + 1; j < l + 1; j++) {
-if((arr[i] + arr[j]) == value) {
-c1++;
+int countPairs1(int* arr, int len, int value) {
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if ((arr[i] + arr[j]) == value) count += 1;
+        }
+    }
+    return count;
 }
+int countPairs2(int* arr, int len, int value) {
+    int count = 0, l = 0, r = len, m;
+    while (l < r - 1) {
+        m = (l + r) / 2;
+        if (arr[m] <= value)
+            l = m;
+        else
+            r = m;
+    }
+    len = r;
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = len; j > i; j--) {
+            if (arr[i] + arr[j] == value)
+                count++;
+        }
+    }
+    return count;
 }
-}
-if(c1 >= 0) {
-return c1;
-} else {
-return 0;
-}
-}
-int countPairs2(int *arr, int len, int value) {
-int rg = len - 1;
-int lg = 0;
-int c2 = 0;
-while(arr[rg] > value) {
-rg--;
-}
-while(lg <= rg) {
-if (((arr[rg] + arr[lg]) == value) && (lg < rg)) {
-c2++;
-lg++;
-} else if (((arr[rg] + arr[lg]) < value) && (lg < rg)) {
-lg++;
-} else {
-rg--;
-lg = 0;
-}
-}
-if(c2 >= 0) {
-return c2;
-} else {
-return 0;
-}
-}
-int countPairs3(int *arr, int len, int value) {
-int c3 = 0;
-int ii = 0;
-int med, index, valuer, l, r;
-while(ii < len - 1) {
-valuer = value - arr[ii];
-l = ii + 1;
-r = len - 1;
-while (l <= r) {
-med = l + (r - l) / 2;
-if (arr[med] > valuer) {
-r = med - 1;
-} else if (arr[med] < valuer) {
-l = med + 1;
-} else {
-index = med;
-break;
-}
-}
-int i = index;
-while ((i > ii) && (index >= 0) && (arr[i] == valuer)) {
-c3++;
-i--;
-}
-i = index + 1;
-while((i < len) && (len > index) && (arr[i] == valuer)) {
-c3++;
-i++;
-}
-ii++;
-return c3;
+int countPairs3(int* arr, int len, int value) {
+    int l = 0;
+    int r = len;
+    int m;
+    while (l < r - 1) {
+        m = (l + r) / 2;
+        if (arr[m] <= value)
+            l = m + 1;
+        else
+            r = m;
+    }
+    len = r;
+    int count = 0;
+    for (int i = 0; i < len - 1; i++) {
+        int l = i;
+        int r = len;
+        while (l < r - 1) {
+            int m = (l + r) / 2;
+            if (arr[i] + arr[m] == value) {
+                count++;
+                int j = m + 1;
+                while (arr[i] + arr[j] == value && j < r) {
+                    count++;
+                    j++;
+                }
+                j = m - 1;
+                while (arr[i] + arr[j] == value && j > l) {
+                    count++;
+                    j--;
+                }
+                break;
+            }
+            if (arr[i] + arr[m] > value)
+                r = m;
+            else
+                l = m;
+        }
+    }
+    return count;
 }
